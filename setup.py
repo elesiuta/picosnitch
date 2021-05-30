22b7e086a7b31a34539c05f4a811baa7e8383d81
eric <elesiuta@gmail.com>
2021-05-30 12:35:57 -0400

    version 0.3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="picosnitch",
-    version="0.3.5",
+    version="0.3.6",
     description="See which processes make remote network connections",
     long_description=long_description,
     long_description_content_type="text/markdown",
