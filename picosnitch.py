a1e46cbe9c165fb597ed423b44e252ec8c9046b4
aschaap <alexander.l.schaap@gmail.com>
2021-11-15 20:51:56 +0000

    Updated copyright to 2021 (#4)
    * Updated copyright to 2021
    
    * Added 2020 back into copyright

--- picosnitch.py
+++ picosnitch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # picosnitch
-# Copyright (C) 2020 Eric Lesiuta
+# Copyright (C) 2020-2021 Eric Lesiuta
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
